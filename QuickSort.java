a1bd62b07c0be72522e144b1bd597a225bb9ff5a
laobo <laobodslr@gmail.com>
2019-08-06 10:30:20 +0800

    QC

--- QuickSort.java
+++ QuickSort.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 /**
@@ -14,33 +15,35 @@ public class QuickSort {
         }
         sc.close();
         int start=0;
-        int end=n
+        int end=n-1;
         funCore(list,n,start,end);
+        System.out.println(Arrays.toString(list));
     }
 
     private static void funCore(int[] list, int n, int start, int end) {
-        int i=start;
-        int j=end;
-        int k=list[i];
-        while(i<j){
-            while(i<j&&list[j]>k){
-                j--;
-            }
-            if(i<j){
-                list[i]=list[j];
-                i++;
-            }
-            while(i<j&&list[i]<k){
-                i++;
-            }
-            if(i<j){
-                list[j]=list[i];
-                j--;
-            }
-            list[i]=k;
-            funCore(list, n, start, i-1);
-            funCore(list, n, i+1, end);
-        }
-        
+      if(start>end){
+          return;
+      }
+      int i=start;
+      int j=end;
+      int k=list[start];
+      while(i<j){
+          while(i<j&&k<list[j]){
+              j--;
+          }
+          if(i<j){
+              list[i++]=list[j];
+          }
+          while(i<j&&k>list[i]){
+              i++;
+          }
+          if(i<j){
+              list[j--]=list[i];
+          }
+          list[i]=k;
+      }
+      funCore(list, n, start, i-1);
+      funCore(list, n, i+1, end);
+
     }
 }
